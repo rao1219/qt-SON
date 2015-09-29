f9ccdb8a9dd3d11229c19a1a64760774d3a6c016
rao1219 <441718399@qq.com>
2015-09-29 20:56:44 +0800

    edit data source

--- timer.cpp
+++ timer.cpp
@@ -10,25 +10,31 @@ timer::timer()
 
 }
 
-void timer::mySlots()
+void timer::mySlots()               //还需要加入相应的处理模块
 {
     QTextStream qout(stdout);
     switch(eventType)
     {
     case 0:
         qout << "\033[0;32m"+eventContent+"\033[0;0m\n";
+        qDebug()<<"负载增大，触发容量自配置";
+        // here+自优化
         break;
     case 1:
         qout<< "\033[0;31m"+eventContent+"\033[0;0m\n";
+        qDebug()<<"负载减小，触发容量自配置";
         break;
     case 2:
         qout<< "\033[0;34m"+eventContent+"\033[0;0m\n";
+        qDebug()<<"触发欠覆盖自优化";
         break;
     case 3:
         qout<< "\033[0;31m"+eventContent+"\033[0;0m\n";
+        qDebug()<<"触发频率自配置";
         break;
     case 4:
         qout<< "\033[0;31m"+eventContent+"\033[0;0m\n";
+        qDebug()<<"触发容量自配置";
         break;
 
     }
